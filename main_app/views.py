6c63bc65392e64490e87917e476b9bd9a5043a5d
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-09 10:46:00 -0400

    wip: Creates Monster index/detail/create routes.

--- main_app/views.py
+++ main_app/views.py
@@ -7,18 +7,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .models import Monster
 
 
 def home(request):
     return render(request, 'home.html')
-  
-  
+
+
 def about(request):
     return render(request, 'about.html')
-  
-  
+
+
 def monsters_index(request):
-    return render(request, 'monsters/index.html')
+    monsters = Monster.objects.all()
+    return render(request, 'monsters/index.html', { 'monsters': monsters})
+
+
+def monsters_detail(request, monster_id):
+    monster = Monster.objects.get(id=monster_id)
+    return render(request, 'monsters/detail.html', {'monster': monster})
+
+
+
 
 
 def signup(request):
@@ -34,3 +44,13 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+
+class MonsterCreate(LoginRequiredMixin, CreateView):
+    model = Monster
+    fields = ['name', 'type', 'description', 'age']
+    success_url = '/monsters/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)

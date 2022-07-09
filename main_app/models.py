9b6f2a7ccded474ed9996f3714d7b96e3be99ccb
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-09 10:44:55 -0400

    wip: Creates Monster model.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
 
-# Create your models here.
+
+class Monster(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    # skills = models.ManyToManyField(Skill)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # def fed_for_today(self):
+    #     return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'cat_id': self.id})

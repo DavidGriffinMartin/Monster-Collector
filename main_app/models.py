e650ea99215f703dfa150dc206fdd029735ea3d2
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-09 11:43:13 -0400

    wip: Adds Feeding model.

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 from datetime import date
 
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
 class Monster(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
@@ -12,11 +19,27 @@ class Monster(models.Model):
     # skills = models.ManyToManyField(Skill)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    # def fed_for_today(self):
-    #     return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'monster_id': self.id})
+      
+      
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    monster = models.ForeignKey(Monster, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']

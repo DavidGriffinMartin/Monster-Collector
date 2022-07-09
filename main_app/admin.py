7400645b170ae234b852e653526d036d4b937127
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-09 12:58:50 -0400

    wip: Adds Skill model access to admin.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Monster, Feeding
+from .models import Monster, Feeding, Skill
 
 
 admin.site.register(Monster)
 admin.site.register(Feeding)
+admin.site.register(Skill)
